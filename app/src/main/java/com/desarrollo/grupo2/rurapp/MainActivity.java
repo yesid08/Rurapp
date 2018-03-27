224c554d997f9dca7e183a649bb613f5789fe9f1
Yesid <yesid.08@hotmail.com>
2018-03-27 08:50:59 -0500

    Primer commit

---
+++ app/src/main/java/com/desarrollo/grupo2/rurapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.desarrollo.grupo2.rurapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
